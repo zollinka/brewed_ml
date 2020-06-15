61ac08ae83ed63a5d3e89d569b47eedea60f66bf
zollinka <zo@zo.pl>
2020-06-15 17:46:44 +0200

    added prediction method

--- app.py
+++ app.py
@@ -2,11 +2,15 @@
 from flask import Flask
 from flask_restful import Api
 from resources.predict import Predict
+from tensorflow.keras.models import load_model
+
+MODEL_PATH = 'MobileNet_F_20'
 
 app = Flask(__name__)
 api = Api(app)
+model = load_model(MODEL_PATH)
 
-api.add_resource(Predict, '/api/predict')
+api.add_resource(Predict, '/api/predict', resource_class_kwargs = {'model':model})
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- resources/predict.py
+++ resources/predict.py
@@ -1,11 +1,41 @@
 from flask_restful import Resource, reqparse
+import numpy as np
+from PIL import Image,ImageOps
+import keras
 
 class Predict(Resource):
-	def __init__(self):
+	def __init__(self, **kwargs):
 		super().__init__()
+		self.model = kwargs['model']
 		self.parser = reqparse.RequestParser()
 		self.parser.add_argument('image_path', required=True)
 	
 	def post(self):
 		img_path = self.parser.parse_args().image_path
-		return {'result':img_path}
+		#self.convert_image(img_path)
+		input_arr = self.get_image_from_path(img_path)
+		prediction = self.model.predict(input_arr)
+		class_p = prediction.argmax(axis=-1)
+		lab_csv = np.loadtxt('labels_v1.csv', dtype=[('EAN','U13'),('Name','S')],delimiter=';',encoding='latin-1')
+		labels = lab_csv['EAN']
+		result_label = labels[class_p]
+		return {'result':result_label[0]}
+
+	def get_image_from_path(self, img_path):
+		image = keras.preprocessing.image.load_img(img_path, target_size=(224,224))
+		input_arr = keras.preprocessing.image.img_to_array(image)
+		input_arr /= 255
+		input_arr = np.array([input_arr])
+		return input_arr
+
+	def convert_image(self,path,mode='RGB'):
+		img = Image.open(path)
+		if hasattr(ImageOps, 'exif_transpose'):
+			img = ImageOps.exif_transpose(img)
+			img = img.convert(mode)
+			img.save(path)
+		return 0
+
+
+	def get_nparray_image_from_request(self):
+		return 'lala'
