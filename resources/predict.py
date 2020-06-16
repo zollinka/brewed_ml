c557bff403686b5450d82c6a35bc298a1a14991c
zollinka <zo@zo.pl>
2020-06-16 11:51:22 +0200

    repaired a bug with exif tag handling

--- resources/predict.py
+++ resources/predict.py
@@ -18,24 +18,42 @@ class Predict(Resource):
 		class_p = prediction.argmax(axis=-1)
 		lab_csv = np.loadtxt('labels_v1.csv', dtype=[('EAN','U13'),('Name','S')],delimiter=';',encoding='latin-1')
 		labels = lab_csv['EAN']
-		result_label = labels[class_p]
+		result_label = labels[class_p + 1]
 		return {'result':result_label[0]}
 
-	def get_image_from_path(self, img_path):
+	def __get_image_from_path(self, img_path):
 		image = keras.preprocessing.image.load_img(img_path, target_size=(224,224))
 		input_arr = keras.preprocessing.image.img_to_array(image)
 		input_arr /= 255
 		input_arr = np.array([input_arr])
 		return input_arr
 
-	def convert_image(self,path,mode='RGB'):
+	def __exif_transpose(self, img):
+		if not img:
+			return img
+		exif_orientation_tag = 274
+		if hasattr(img, "_getexif") and isinstance(img._getexif(), dict) and exif_orientation_tag in img._getexif():
+			exif_data = img._getexif()
+			orientation = exif_data[exif_orientation_tag]
+			if orientation == 1 or orientation == 2 or orientation == 4 or orientation == 5 or orientation == 7:
+				pass
+			elif orientation == 3:
+				img = img.rotate(180)
+			elif orientation == 6:
+				img = img.rotate(-90, expand=True)
+			elif orientation == 8:
+				img = img.rotate(90, expand=True)
+
+	def __convert_image(self,path,mode='RGB'):
 		img = Image.open(path)
 		if hasattr(ImageOps, 'exif_transpose'):
 			img = ImageOps.exif_transpose(img)
-			img = img.convert(mode)
-			img.save(path)
+		else:
+			img = exif_transpose(img)
+		img = img.convert(mode)
+		img.save(path)
 		return 0
 
 
-	def get_nparray_image_from_request(self):
+	def __save_image_from_request(self):
 		return 'lala'
