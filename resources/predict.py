e28f435ead97782bfbcd56ddfc7478161d0eb4e0
zollinka <zo@zo.pl>
2020-06-16 19:04:01 +0200

    added request with file handling

--- resources/predict.py
+++ resources/predict.py
@@ -2,33 +2,43 @@ from flask_restful import Resource, reqparse
 import numpy as np
 from PIL import Image,ImageOps
 import keras
+from flask import request
+import werkzeug
 
 class Predict(Resource):
 	def __init__(self, **kwargs):
 		super().__init__()
 		self.model = kwargs['model']
 		self.parser = reqparse.RequestParser()
-		self.parser.add_argument('image_path', required=True)
-	
+		#self.parser.add_argument('image_path', required=True)
+		self.parser.add_argument('image',required=True, type=werkzeug.datastructures.FileStorage, location='files')
 	def post(self):
-		img_path = self.parser.parse_args().image_path
-		self.convert_image(img_path)
-		input_arr = self.get_image_from_path(img_path)
-		prediction = self.model.predict(input_arr)
-		class_p = prediction.argmax(axis=-1)
-		lab_csv = np.loadtxt('labels_v1.csv', dtype=[('EAN','U13'),('Name','S')],delimiter=';',encoding='latin-1')
-		labels = lab_csv['EAN']
-		result_label = labels[class_p + 1]
-		return {'result':result_label[0]}
+		try:
+			inp_image = self.parser.parse_args().image
+			img_path = 'predict_image.jpg'
+			inp_image.save(img_path)
+			self.convert_image(img_path)
+			img = self.get_image_from_path(img_path)
+			prediction = self.model.predict(img)
+			class_p = prediction.argmax(axis=-1)
+			lab_csv = np.loadtxt('labels_v1.csv', dtype=[('EAN','U13'),('Name','S')],delimiter=';',encoding='latin-1')
+			labels = lab_csv['EAN']
+			result_label = labels[class_p + 1]
+			t = 'success'
+			#return {'result': t, 'label': '0'} #result_label[0]}
+		except Exception as e:
+			t = 'fail'
+			return {'result': t}
+		return {'result': t, 'label': result_label[0]}
 
-	def __get_image_from_path(self, img_path):
+	def get_image_from_path(self, img_path):
 		image = keras.preprocessing.image.load_img(img_path, target_size=(224,224))
 		input_arr = keras.preprocessing.image.img_to_array(image)
 		input_arr /= 255
 		input_arr = np.array([input_arr])
 		return input_arr
 
-	def __exif_transpose(self, img):
+	def exif_transpose(self, img):
 		if not img:
 			return img
 		exif_orientation_tag = 274
@@ -44,7 +54,7 @@ class Predict(Resource):
 			elif orientation == 8:
 				img = img.rotate(90, expand=True)
 
-	def __convert_image(self,path,mode='RGB'):
+	def convert_image(self,path,mode='RGB'):
 		img = Image.open(path)
 		if hasattr(ImageOps, 'exif_transpose'):
 			img = ImageOps.exif_transpose(img)
@@ -55,5 +65,5 @@ class Predict(Resource):
 		return 0
 
 
-	def __save_image_from_request(self):
+	def save_image_from_request(self):
 		return 'lala'
